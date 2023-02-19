5bffe42ae7cce111beba95655a6ba390af6134e6
dule88 <d.dimitrijevic88@gmail.com>
2023-02-19 19:27:46 +0100

    Fixed pages About us and Contact us

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+        <section className="about container">
+        <h2>About us</h2>
+        <article>
+          <div>
+            <img src="img/about.jpg" alt=""/>
+          </div>
+          <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique et maiores inventore perferendis sed maxime ab, recusandae minima eaque, temporibus sequi commodi! Eveniet optio, dignissimos dicta mollitia earum iste, at atque obcaecati
+              quod dolores neque aut consectetur ad dolor quibusdam!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique et maiores inventore perferendis sed maxime ab, recusandae minima eaque, temporibus sequi commodi! Eveniet optio, dignissimos dicta mollitia earum iste, at atque obcaecati
+              quod dolores neque aut consectetur ad dolor quibusdam!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique et maiores inventore perferendis sed maxime ab, recusandae minima eaque, temporibus sequi commodi! Eveniet optio, dignissimos dicta mollitia earum iste, at atque obcaecati
+              quod dolores neque aut consectetur ad dolor quibusdam!</p>
+            <hr/>
+              <div>
+                <span>Find us on:</span>
+                <span><a href=""><i className="fab fa-facebook-square"></i></a></span>
+                <span><a href=""><i className="fab fa-instagram"></i></a></span>
+                <span><a href=""><i className="fab fa-twitter"></i></a></span>
+                <span><a href=""><i className="fab fa-pinterest"></i></a></span>
+              </div>
+          </div>
+        </article>
+
+      </section>
+    </>
   )
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+        <section className="contact container">
+        <h2>Contact us</h2>
+        <article>
+          <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2808.530189651408!2d19.848169715720957!3d45.257292279099154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475b1066090a7219%3A0xf05b709a8e7ae221!2z0KLRgNCzINCg0LXQv9GD0LHQu9C40LrQtSAyMCwg0J3QvtCy0Lgg0KHQsNC0IDIxMDAw!5e0!3m2!1ssr!2srs!4v1622578707051!5m2!1ssr!2srs" width="100%" height="500" style={{border:'0'}} allowFullScreen="" loading="lazy"></iframe>
+          </div>
+
+          <div>
+            <form>
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email" />
+              <textarea cols="30" rows="10" placeholder="Message....."></textarea>
+              <button>Send message</button>
+            </form>
+            <hr />
+            <div>
+              <span>Find us on:</span>
+              <span><a href=""><i className="fab fa-facebook-square"></i></a></span>
+              <span><a href=""><i className="fab fa-instagram"></i></a></span>
+              <span><a href=""><i className="fab fa-twitter"></i></a></span>
+              <span><a href=""><i className="fab fa-pinterest"></i></a></span>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
   )
 }
 
-export default Contact
+export default Contact;
