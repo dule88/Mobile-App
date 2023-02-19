35ab3654eab85d7f297f7ace354153af84d29eb7
dule88 <d.dimitrijevic88@gmail.com>
2023-02-19 18:43:33 +0100

    Fixed navigation and static Home page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,40 @@ import Topbar from './components/Topbar';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+// Pages
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Single from './pages/Single';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Admin from './pages/Admin';
+import NotFound from './pages/NotFound';
 
-function App() {
+
+import { Routes, Route } from 'react-router-dom';
+
+
+
+const App = () => {
   return (
     <div className="App">
 
           <Topbar />
           <Navbar />
-     
+
+          <Routes>
+
+            <Route path='/' element={<Home/>}/>
+            <Route path='/products' element={<Products/>}/>
+            <Route path='/single' element={<Single/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/admin' element={<Admin/>}/>
+            <Route path='/*' element={<NotFound/>}/>
+
+          </Routes>
 
           <Footer />
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroHeder from '../components/HeroHeder';
+import Subscribe from '../components/Subscribe';
+
+const Home = () => {
+  return (
+    <>
+        <HeroHeder/>
+        <Subscribe/>
+    </>
+  )
+}
+
+export default Home;
