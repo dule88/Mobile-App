e8fc2576d9c72077a9fd1d6319d8b24c46450c8f
dule88 <d.dimitrijevic88@gmail.com>
2023-02-20 19:11:33 +0100

    Cart page is fixed and dynamic

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,11 +7,29 @@ const Cart = () => {
   
   const {cart, setCart} = useContext(CartContext); 
 
+  const removeItem = (idx) => {
+    let tempCart = [...cart];
+    tempCart.splice(idx, 1);
+    setCart([...tempCart]);
+  }
+
+  const clearCart = () => {
+    setCart([]);
+  } 
+
+// Calculating the sum of products in the cart
+
+  let total = cart.reduce((acc, curVal) => {
+    return acc + curVal.qty * curVal.price;
+  }, 0);
 
 
 
   return (
     <div className='container'>
+        {
+          cart.length > 0
+          ?
           <table className='table table-striped'>
             <thead>
               <tr>
@@ -19,7 +37,7 @@ const Cart = () => {
                 <th scope="col">Image</th>
                 <th scope="col">Name</th>
                 <th scope="col">Unit Price</th>
-                <th scope="col">Qty.</th>
+                <th scope="col">Qty</th>
                 <th scope="col">Price</th>
                 <th scope="col">Remove</th>
               </tr>
@@ -27,31 +45,32 @@ const Cart = () => {
             <tbody>
               {cart.map((item, idx) => {
                 return (
-                  <tr>
-                    <th scope="row">{item.id}</th>
-                    <td><img src={`http://localhost:3000/${item.img}`} alt="Photo" height="30px" /></td>
-                    <td>{item.name}</td>
-                    <td>${item.price}</td>
-                    <td>{item.qty}</td>
-                    <td>${item.price * item.qty}</td>
-                    <td><button class="btn btn-danger">X</button></td>
+                  <tr >
+                    <td className='py-3'><img src={`http://localhost:3000/${item.img}`} alt="Photo" height="50px" /></td>
+                    <th className='py-3'>{item.id}</th>
+                    <td className='py-3'>{item.name}</td>
+                    <td className='py-3'>${item.price}</td>
+                    <td className='py-3'>{item.qty}</td>
+                    <td className='py-3'>${item.price * item.qty}</td>
+                    <td className='py-3'><button className="btn btn-danger my-auto" onClick={() => removeItem(idx)}>X</button></td>
                   </tr>
                 );
               })}
 
-              <tr>
+              <tr className='m-5'>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
-                <td>total:</td>
+                <td className='py-2'>Total Sum: ${total}</td>
                 <td></td>
-                <td><button class="btn btn-danger">Clear cart</button></td>
+                <td className='py-2'><button className="btn btn-danger my-auto" onClick={() => clearCart()}>Clear cart</button></td>
               </tr>
             </tbody>
           </table>
-          
-          <h4>There are no products on the cart.</h4>
+          :
+          <h4>There are no products in the cart.</h4>
+        }
     </div>
   )
 }
